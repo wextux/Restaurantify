b3a1f2e1b677612f4c261d036e24ebde402a19e8
Will Fairclough <willbf6@gmail.com>
2011-11-04 11:21:29 -0400

    Organized project stucture

---
+++ Restaurantify/Classes/BYAppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  BYAppDelegate.h
+//  Restaurantify
+//
+//  Created by Will Fairclough on 11-11-03.
+//  Copyright (c) 2011 Superna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LLStoreWrapper.h"
+
+@class BYViewController;
+
+@interface BYAppDelegate : UIResponder <UIApplicationDelegate, LLStoreDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BYViewController *viewController;
+
+@end
---
+++ Restaurantify/Classes/BYViewController.h
@@ -0,0 +1,13 @@
+//
+//  BYViewController.h
+//  Restaurantify
+//
+//  Created by Will Fairclough on 11-11-03.
+//  Copyright (c) 2011 Superna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BYViewController : UIViewController
+
+@end
