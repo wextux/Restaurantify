eb8fc9b6034fe1af3e7dcd6c7358a3ff295ca30a
Will Fairclough <willbf6@gmail.com>
2011-11-05 12:42:21 -0400

    Added/Changed Comments for classes

--- Restaurantify/Classes/BYViewController.m
+++ Restaurantify/Classes/BYViewController.m
@@ -3,7 +3,7 @@
 //  Restaurantify
 //
 //  Created by Will Fairclough on 11-11-03.
-//  Copyright (c) 2011 Superna. All rights reserved.
+//  Copyright (c) 2011 Boneyard Interactive. All rights reserved.
 //
 
 #import "BYViewController.h"
