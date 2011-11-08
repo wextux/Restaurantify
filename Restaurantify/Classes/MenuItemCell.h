80ad7cef7fd48f46f4972ab6f46005fcee24f2ae
Will Fairclough <willbf6@gmail.com>
2011-11-08 11:41:49 -0500

    Cleaned up code and comments

--- Restaurantify/Classes/MenuItemCell.h
+++ Restaurantify/Classes/MenuItemCell.h
@@ -3,7 +3,7 @@
 //  Restaurantify
 //
 //  Created by Will Fairclough on 11-11-05.
-//  Copyright (c) 2011 Superna. All rights reserved.
+//  Copyright (c) 2011 Boneyard Interactive. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
